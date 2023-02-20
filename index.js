ea61d9b528d36a1b06a159edea827e77476685d0
amiekisiak <117371691+amiekisiak@users.noreply.github.com>
2023-02-20 19:14:47 +0000

    added ExampleREADME file

--- index.js
+++ index.js
@@ -21,62 +21,65 @@ function generateReadme() {
       type: 'input',
       message: 'If required, please describe installation process?',
       name: 'installation',
-      
+
     },
     {
       type: 'input',
       message: 'If required, provide an example of your project in use ?',
       name: 'usage',
-      
+
     },
     {
       type: 'input',
-      message: 'Please enter link to deployed application:',
-      name: 'deployment',
+      message: 'Please add screenshot of the deployed application?',
+      name: 'screenshot',
     },
+
     {
       type: 'input',
       message: 'Please provide examples of tests written for your application if any?',
       name: 'tests',
-      
+
     },
     {
       type: 'list',
       message: "Please choose license for your project.",
-      choices: ['GNU GPLv3', 'GNU LGPLv3', 'Eclipse Public License 2.0','Creative Commons Zero v1.0 Universal', 'Apache License 2.0', 'MIT License', 'Mozilla Public License 2.0','Boost Software License 1.0', 'The Unlicense'],
+      choices: ['GNU GPLv3', 'GNU LGPLv3', 'Eclipse Public License 2.0', 'Creative Commons Zero v1.0 Universal', 'Apache License 2.0', 'MIT License', 'Mozilla Public License 2.0', 'Boost Software License 1.0', 'The Unlicense'],
       name: 'license'
     },
     {
       type: 'input',
       message: 'List of contributors',
       name: 'contributing',
-  },
-  {
-    type: 'input',
-    message: 'Please enter your email:',
-    name: 'email',
-},
-{
-  type: 'input',
-  message: 'Please enter link to your GitHub:',
-  name: 'github',
-},
-
-
+    },
+    {
+      type: 'input',
+      message: 'Please enter your email:',
+      name: 'email',
+    },
+    {
+      type: 'input',
+      message: 'Please enter your GitHub user name:',
+      name: 'github',
+    },
+    {
+      type: 'input',
+      message: 'Please enter link to deployed application:',
+      name: 'deploy',
+    },
   ])
-    .then((responses) => {
-     
-      const markdown = generateMarkdown(responses);
-      fs.writeFile('README.md', markdown, (err) => {
-        if (err) {
-          console.error(err);
-        } else {
-          console.log('README.md generated!');
-        }
-      });
+  .then((data) => {
+    const markdown = generateMarkdown(data);
+    fs.writeFile('README.md', markdown, (err) => {
+      if (err) {
+        console.error(err);
+      } else {
+        console.log('README.md generated!');
+      }
     });
+  });
 }
 
 module.exports = generateReadme;
 
-generateReadme(); // Call the function to generate the README file
+generateReadme(); 
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,46 +1,57 @@
-// Function generating data to README file based on user's input
 function generateMarkdown(data) {
-    let licenseBadge = "";
-    let licenseLink = "";
-    let licenseSection = "";
-      
-    if (data.license !== "None") {
-      licenseBadge = `![Badge](https://img.shields.io/badge/License-${data.license}-green.svg)`;
-      licenseLink = `- [License](#license-)`;
-      licenseSection = `## License
+  let licenseBadge = "";
+  let licenseLink = "";
+  let licenseSection = "";
+
+  if (data.license && data.license !== "None") {
+    licenseBadge = `![Badge](https://img.shields.io/badge/License-${data.license}-green.svg)`;
+    licenseLink = `- [License](#license-)`;
+    licenseSection = `
+  ## License
   This application is licensed under ${data.license} license.`;
-    }
-      
-    return `
+  } else {
+    licenseSection = `
+  ## License
+  This application is not licensed.`;
+  }
+
+  return `
   # ${data.title}
   ${licenseBadge}
-  ${data.name}
-  ## Description
+  
+  ## Description 
   ${data.description}
-  ## Table of Contents
+  
+  ## Table of Contents 
   - [Installation](#installation)
   - [Usage](#usage)
+  - [Screenshot](#screenshot)
   ${licenseLink}
   - [Test](#tests)
   - [Credits](#credits)
   - [Questions](#questions)
   - [Deployed Application](#deployed-application)
+  
   ## Installation
   ${data.installation}
+  
   ## Usage
   ${data.usage}
+  
   ${licenseSection}
-  ## Deployment
-  This application was deployed at [${data.deployedLink}](${data.deployedLink})
+  
   ## Tests
   ${data.tests}
+  
   ## Contributors
   ${data.contributing}
+  
   ## Questions
   Please feel free to email me at: ${data.email}
-  My GitHub: [${data.github}](https://www.github.com/${data.github})
+  My GitHub: [${data.github}](${data.github})
+  
+  ## Deployed Application
+  This application was deplopyed at [${data.deploy}](${data.deploy})
   `;
-  }
-  
-  module.exports = generateMarkdown;
-  
+}
+module.exports = generateMarkdown;
